44b64fba88d33fe22514b1de7f31c3c899516a1c
Steven <unklfoddy@gmail.com>
2025-01-13 16:43:13 -0500

    in progress

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*  for future reference
+
+let shoppingList = ["Apricots", "Pineapple", "Raisins"];
+console.log(shoppingList[0]);    // Output: "Apricots"
+
+shoppingList.shift();    // Removing the first element with the shift method
+console.log(shoppingList);   // Output: ["Pineapple", "Raisins"]
+
+shoppingList.push("Cashews", "Cantelope");
+console.log(shoppingList); 
+
+shoppingList.unshift("Peaches");
+console.log(shoppingList); 
+
+shoppingList.splice(2,0,"Apricots");
+console.log(shoppingList); 
+
+shoppingList.pop();
+console.log(shoppingList); 
+
+shoppingList.splice(3,1);
+console.log(shoppingList); 
+
+delete shoppingList[2];
+console.log(shoppingList); 
+
+shoppingList.splice(2,0,"Apples");
+console.log(shoppingList);
+
+shoppingList[3] = "Bananas";
+console.log(shoppingList);
+
+if (shoppingList[3]) {
+    console.log("Yes, we have some " + shoppingList[3] + "!  We have some " + shoppingList[3] + " today!");
+}
+
+
+
+console.log (shoppingList.length);
+ 
+ for (let i = 0; shoppingList.length; i++) {
+    console.log("Yes, we have some " + shoppingList[i] + "!  We have some " + shoppingList[i] + " today!");
+};   THIS CREATES AN ENDLESS LOOP, FIND OUT WHY */
