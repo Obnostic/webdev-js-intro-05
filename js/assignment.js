0c6c1ff00c230f7a6347eb34e6b58be36adf2b47
Steven <unklfoddy@gmail.com>
2025-01-15 15:10:58 -0500

    AIEEEEE

--- js/assignment.js
+++ js/assignment.js
@@ -9,6 +9,8 @@ const todos = [
 ];
 
 // Declare your two immutable variables
+let shiftValue;
+let popValue;
 
 
 // Declaring constant variables to store references to the elements
@@ -21,47 +23,77 @@ const answerFiveEl = document.getElementById("answer-five");
 const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
+console.log(`Answer1: ${answerOneEl}`);
+
 function updateAnswerOne() {
-    
+    answerOneEl.innerHTML(todos);   
 }
 
 function updateAnswerTwo() {
-
+    answerTwoEl.innerText(todos[2]); 
 }
 
 function removeLastValue() {
-
+    let popvalue = todos.pop();
 }
 
 function removeFirstValue() {
-
+    let shiftValue = todos.shift();    
 }
 
 function addShiftAndPopValues() {
-
+    todos.push(popValue, shiftValue);
 }
 
 function updateAnswerFour() {
-
+    console.log(todos);
 }
 
 function reverseTodoList() {
+    todos.reverse();
 
 }
 
 function updateAnswerFive() {
-
+    console.log(todos);
 }
 
 function updateAnswerSix() {
-
+    console.log(todos);
 }
 
 function render() {
-
+    console.log(todos);
 }
+
+submissionBtn.addEventListener("click", function () {
+   /* console.log('Window'); */
+   console.log("answer-one"); 
+     render();    
+});  
+
+/*
+
+orig was:
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
     render();
-});
+});  
+
+
+
+
+window.addEventListener("click", function() {
+	console.log('Window');
+    } , true);
+
+document.addEventListener("click", function() {
+	console.log('Document');
+    } , true);
+
+document.querySelector('submission-btn').addEventListener
+	("click", function(e) {
+		console.log(e.target.innerText = 'Submitted!');
+    } , true);
+*/
--- js/index.js
+++ js/index.js
@@ -2,46 +2,48 @@
 // that are not declared
 "use strict"
 
-/*  for future reference
+/* for future reference */
 
 let shoppingList = ["Apricots", "Pineapple", "Raisins"];
 console.log(shoppingList[0]);    // Output: "Apricots"
 
-shoppingList.shift();    // Removing the first element with the shift method
-console.log(shoppingList);   // Output: ["Pineapple", "Raisins"]
+shoppingList.shift();            // Removing the first element with the shift method
+console.log(shoppingList);       // Output: ["Pineapple", "Raisins"]
 
-shoppingList.push("Cashews", "Cantelope");
-console.log(shoppingList); 
+shoppingList.push("Cashews", "Cantelope");  // Add two new elements to the end
+console.log(shoppingList);       // Output:   ['Pineapple', 'Raisins', 'Cashews', 'Cantelope']
 
-shoppingList.unshift("Peaches");
-console.log(shoppingList); 
+shoppingList.unshift("Peaches"); // Add a new element to the beginning of the list
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', 'Raisins', 'Cashews', 'Cantelope']
 
-shoppingList.splice(2,0,"Apricots");
-console.log(shoppingList); 
+shoppingList.splice(2,0,"Apricots"); // Insert a new element between the 2nd & 3rd elements
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', 'Apricots', 'Raisins', 'Cashews', 'Cantelope']
 
-shoppingList.pop();
-console.log(shoppingList); 
+shoppingList.pop();              // Remove element from the end of the array
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', 'Apricots', 'Raisins', 'Cashews']
 
-shoppingList.splice(3,1);
-console.log(shoppingList); 
+shoppingList.splice(3,1);        // Go to the 4th element and remove it (cell count starts at zero)
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', 'Apricots', 'Cashews']   
 
-delete shoppingList[2];
-console.log(shoppingList); 
+delete shoppingList[2];          // Erase the contents of Element 3 but do not remove it
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', …, 'Cashews']
+                                 // Element 3 is rendered null or undefined
 
-shoppingList.splice(2,0,"Apples");
-console.log(shoppingList);
 
-shoppingList[3] = "Bananas";
-console.log(shoppingList);
+shoppingList.splice(2,0,"Apples");  //Insert new item after the 2nd element.  Delete no elements.
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', 'Apples', …, 'Cashews']
+
+shoppingList[3] = "Bananas";     // Make new assignment to Element 4
+console.log(shoppingList);       // Output:  ['Peaches', 'Pineapple', 'Apples', 'Bananas', 'Cashews']
 
 if (shoppingList[3]) {
-    console.log("Yes, we have some " + shoppingList[3] + "!  We have some " + shoppingList[3] + " today!");
-}
+    console.log(`Yes, we have some ${shoppingList[3]}!  We have some ${shoppingList[3]} today!`);
+}                                // Output in the template literals format.
+        // Output:  Yes, we have some Bananas !  We have some Bananas today!
 
 
-
-console.log (shoppingList.length);
+console.log (shoppingList.length);  // Output:  5
  
- for (let i = 0; shoppingList.length; i++) {
-    console.log("Yes, we have some " + shoppingList[i] + "!  We have some " + shoppingList[i] + " today!");
-};   THIS CREATES AN ENDLESS LOOP, FIND OUT WHY */
+ for (let i = 0; i < shoppingList.length; i++) {
+    console.log(`Yes, we have some ${shoppingList[i]}!  We have some ${shoppingList[i]} today!`);
+};                             // same as above but running through the whole array
